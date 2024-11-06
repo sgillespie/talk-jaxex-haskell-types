c84d1859702106fd63a3afc7136318edfb5cf5fd
Sean D Gillespie <sean@mistersg.net>
2024-11-05 22:42:02 -0500

    Add some more examples

--- src/Development/Examples/Basics.hs
+++ src/Development/Examples/Basics.hs
@@ -1,17 +1,91 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module Development.Examples.Basics where
 
-import Data.List.NonEmpty (NonEmpty (..))
+import Data.Char (digitToInt)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Prelude hiding (head, id)
 
--- Head from base is a partial function
+-- In Haskell, type annotations are usually written outside the function
+id :: a -> a
+id x = x
+
 head :: [a] -> a
 head (x : xs) = x
 head [] = error "Empty list"
 
--- A safer option is to encode failure into the type
-headMay :: [a] -> Maybe a
-headMay (x : xs) = Just x
-headMay [] = Nothing
+-- But we can also write them inline
+i = 0 :: Int
+is = [1 ..] :: [Int]
 
--- An even better way is to use a non-empty list, enforced at compile time
-headNonEmpty :: NonEmpty a -> a
-headNonEmpty (x :| xs) = x
+-- * Simple Types
+i' :: Int
+i' = 0
+
+d :: Double
+d = 0.1
+
+c :: Char
+c = 'H'
+
+-- * Function types
+f :: Char -> Int
+f = digitToInt
+
+g :: Int -> Int
+g = (+) 1
+
+h :: Int -> Int -> Int
+h = (+)
+
+-- * Type Constructors
+a :: [Int]
+a = [0]
+
+b :: Map Char Int
+b = Map.empty
+
+-- * Polymorphic Types
+
+-- Type variables can stand for any type
+f' :: forall a. a
+f' = error "Uh oh!"
+
+g' :: forall a. a -> a
+g' = id
+
+h' :: forall k a. Map k a
+h' = Map.empty
+
+-- * Typeclasses
+
+-- Allow us to create overloaded functions
+class T a where
+  t :: a -> a
+
+instance (T Int) where
+  t x = x + 1
+
+-- * User defined types
+
+-- Type aliases
+type MyString = String
+
+-- Algebraic Data Types
+data MySum
+  = MyInt Int
+  | MyDouble Double
+  deriving (Eq, Show)
+
+data MyProduct a = MyProduct Int Double [a]
+
+data MyRecord a = MyRecord
+  { fieldA :: String,
+    fieldB :: Int,
+    fieldC :: [a]
+  }
+  deriving (Eq, Show)
+
+newtype IntWrapper = IntWrapper {unMyInt :: Int}
+  deriving (Eq, Show)
---
+++ src/Development/Examples/Lists.hs
@@ -0,0 +1,17 @@
+module Development.Examples.Lists where
+
+import Data.List.NonEmpty (NonEmpty (..))
+
+-- Head from base is a partial function
+head :: [a] -> a
+head (x : xs) = x
+head [] = error "Empty list"
+
+-- A safer option is to encode failure into the type
+headMay :: [a] -> Maybe a
+headMay (x : xs) = Just x
+headMay [] = Nothing
+
+-- An even better way is to use a non-empty list, enforced at compile time
+headNonEmpty :: NonEmpty a -> a
+headNonEmpty (x :| xs) = x
